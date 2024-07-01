09eef12d92f942b6e2310a50999a3769e53e2218
jorisfietje <joris.fietje@gmail.com>
2024-07-01 20:53:54 +0200

    Laatste updates

--- src/test/java/controller/CalculatorServletTest.java
+++ src/test/java/controller/CalculatorServletTest.java
@@ -1,37 +1,25 @@
 package controller;
 
+import org.junit.Before;
 import org.junit.Test;
-
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorServletTest {
+    private CalculatorServlet calculatorServlet;
 
+    @Before
+    public void setUp() {
+        calculatorServlet = new CalculatorServlet();
+    }
 
     @Test
     public void testCalculateLotSize() {
-        // Sample input values
-        String valutapaar = "GBPEUR";
-        double inlegbedrag = 5000;
+        double saldo = 5000;
         double risicoPercentage = 100;
         double exchangeRate = 1.18;
-        double stopLoss = 10.0;
-
-        // Expected result
-        double tradedmoney = inlegbedrag * (risicoPercentage / 100);
-        System.out.println("Traded Money: " + tradedmoney);
-        double exchange = tradedmoney / exchangeRate;
-        System.out.println("GBP price: " + exchange);
-        double lotsize = exchange / 1000;
-        System.out.println("lotsize: " + lotsize);
-
-        // Test calculateLotSize method
-        CalculatorServlet calculatorServlet = new CalculatorServlet();
-       // double lotSize = calculatorServlet.calculateLotSize(inlegbedrag, risicoPercentage, exchangeRate);
-
-//        System.out.println("Lot Size: " + lotSize);
-        // Assert
-       // assertEquals(exchange, lotSize, 0.01);
+        double expectedLotSize = (saldo * (risicoPercentage / 100)) / (exchangeRate * 1000);
+        double actualLotSize = calculatorServlet.calculateLotSize(saldo, risicoPercentage, exchangeRate);
+        assertEquals(expectedLotSize, actualLotSize, 0.01);
     }
-
 }
